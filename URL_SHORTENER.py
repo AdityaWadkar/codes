ebcda19ec1f6336d13d75c4042ac64acd7cc21cb
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2022-10-16 10:46:06 +0530

    URL_SHORTENER.py
    This script will simply reduce url size

---
+++ URL_SHORTENER.py
@@ -0,0 +1,10 @@
+# This script will simply reduce url size
+"""
+requirements 
+
+To install pyshorteners just grab it directly from PyPI:
+pip install pyshorteners
+"""
+import pyshorteners as p
+link="Your link here"
+print(p.Shortener().clckru.short(link))
